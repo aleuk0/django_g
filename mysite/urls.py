278073ccfbef5e793aaf9adb48ffb2ce03825e6a
Alexander Kozarev <al1102@yandex.ru>
2016-10-24 01:11:37 +0600

    Little changes.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),  #все запросы начинающиеся с admin будут отправляться на страницу admin'a
+    url(r'', include('blog.urls')), #остальные запросы будут отправляться в приложение blog
 ]
